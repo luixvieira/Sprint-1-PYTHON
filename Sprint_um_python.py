064c1242b3e76bb3c1f07cc99edf1a0304684e7e
luixvieira <161535399+luixvieira@users.noreply.github.com>
2024-04-13 23:32:23 -0300

    Add files via upload

---
+++ Sprint_um_python.py
@@ -0,0 +1,10 @@
+# Sistema de Pré e Auto-atendimento de problemas em carros
+
+# Criando a função exibir menu, quando eu chama-lá pelo nome da função, ela rodará.
+
+def exibir_menu():
+    print("Bem-vindo ao Sistema de Pré-Atendimento para Carros!")
+    print("Escolha uma opção:")
+    print("1. Registrar problemas do carro")
+    print("2. Realizar diagnóstico")
+    print("3. Sair")
